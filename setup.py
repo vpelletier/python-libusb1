b89347a6aaec2f5d74f788b966ead5cc8a2ae37e
Andrew Straw <strawman@astraw.com>
2010-08-08 14:13:14 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='python-libusb1',
+      description='a Python wrapper around libusb-1.0',
+      version='0.0.1',
+      author='vpelletier',
+      url='http://github.com/vpelletier/python-libusb1',
+      py_modules=['libusb1','usb1'],
+      )
