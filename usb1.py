3c631dea80c51680c20c687a2e61f3411f464709
Vincent Pelletier <plr.vincent@gmail.com>
2010-09-12 20:39:43 +0200

    Redesign asynchronous API to be more python-ish.
    - libusb1 transfers are freed automatically (they were never freed in
      previous API unless one used the libusb1 function directly)
    - there is a single way to get a transfer instance, only multiple ways to
      fill it
    - async classes are not only for reading, but can be used to write too
    - use string_at to get buffer data from transfer object rather than by
      keeping a reference to original buffer
    - don't put transfer objects in user's hands, they must go through
      USBTransfer API now
    
    Also, bump version number to denote the API change.

--- usb1.py
+++ usb1.py
@@ -17,17 +17,17 @@ Features:
   Note: Isochronous support is not implemented yet.
 - Asyncrhonous I/O (control, bulk, interrupt)
   Note: Isochronous support is not implemented yet.
-  Helper classes are provided for asynchronous reading (USBPoller,
-  USBAsync(Bulk|Interrupt)Reader).
+  See USBPoller, USBTransfer and USBTransferHelper.
 """
 
 import libusb1
 from ctypes import byref, create_string_buffer, c_int, sizeof, POINTER, \
-    create_unicode_buffer, c_wchar, cast, c_uint16, c_ubyte
+    create_unicode_buffer, c_wchar, cast, c_uint16, c_ubyte, string_at, \
+    addressof
 from cStringIO import StringIO
 
 __all__ = ['LibUSBContext', 'USBDeviceHandle', 'USBDevice',
-    'USBPoller', 'USBAsyncBulkReader', 'USBAsyncInterruptReader']
+    'USBPoller', 'USBTransfer', 'USBTransferHelper', 'EVENT_CALLBACK_SET']
 
 # Default string length
 # From a comment in libusb-1.0: "Some devices choke on size > 255"
@@ -45,61 +45,292 @@ EVENT_CALLBACK_SET = frozenset((
 
 DEFAULT_ASYNC_TRANSFER_ERROR_CALLBACK = lambda x, y: False
 
-class USBAsyncReaderBase(object):
+class USBTransfer(object):
     """
-    Base class for asynchronous readers.
-    Simplifies subscribing to the same transfer over and over.
+    USB asynchronous transfer control & data.
 
-    Callbacks used in this class will be given 2 parameters:
-    - a libusb1.libusb_transfer structure
-    - a buffer with received data (if any)
-    It is expected to return a boolean value:
-    - True if transfer is to be submitted immediately (to receive more data)
+    All modification methods will raise if called on a submitted transfer.
+    Methods noted as "should not be called on a submitted transfer" will not
+    prevent you from reading, but returned value is unspecified.
+    """
+    # Prevent garbage collector from freeing the free function before our
+    # instances, as we need it to property destruct them.
+    __libusb_free_transfer = libusb1.libusb_free_transfer
+    __transfer = None
+    __initialized = False
+    __submitted = False
+    __callback = None
+    __ctypesCallbackWrapper = None
+
+    def __init__(self, handle, iso_packets=0):
+        """
+        You should not instanciate this class directly.
+        Call "getTransfer" method on an USBDeviceHandle instance to get
+        instances of this class.
+        """
+        self.__handle = handle
+        result = libusb1.libusb_alloc_transfer(iso_packets)
+        if not result:
+            raise libusb1.USBError, 'Unable to get a transfer object'
+        self.__transfer = result
+        self.__ctypesCallbackWrapper = libusb1.libusb_transfer_cb_fn_p(
+            self.__callbackWrapper)
+
+    def close(self):
+        """
+        Stop using this transfer.
+        This removes some references to other python objects, to help garbage
+        collection.
+        Raises if called on a submitted transfer.
+        This does not prevent future reuse of instance (calling one of
+        "setControl", "setBulk", "setInterrupt" or "setIsochronous" methods
+        will initialize it properly again), just makes it ready to be
+        garbage-collected.
+        It is not mandatory to call it either, if you have no problems with
+        garbage collection.
+        """
+        if self.__submitted:
+            raise ValueError, 'Cannot close a submitted transfer'
+        self.__initialized = False
+        self.__callback = None
+
+    def __del__(self):
+        if self.__transfer is not None:
+            try:
+                try:
+                    self.cancel()
+                except libusb1.USBError, exception:
+                    if exception.value != libusb1.LIBUSB_ERROR_NOT_FOUND:
+                        raise
+            finally:
+                self.__libusb_free_transfer(self.__transfer)
+
+    def __callbackWrapper(self, transfer_p):
+        """
+        Makes it possible for user-provided callback to alter transfer when
+        fired (ie, mark transfer as not submitted upon call).
+        """
+        mine = addressof(self.__transfer.contents)
+        his = addressof(transfer_p.contents)
+        assert mine == his, (mine, his)
+        self.__submitted = False
+        self.__callback(self)
+
+    def setCallback(self, callback):
+        """
+        Change transfer's callback.
+        """
+        if self.__submitted:
+            raise ValueError, 'Cannot alter a submitted transfer'
+        self.__callback = callback
+
+    def getCallback(self):
+        """
+        Get currently set callback.
+        """
+        return self.__callback
+
+    def setControl(self, request_type, request, value, index, buffer_or_len,
+            callback=None, user_data=None, timeout=0):
+        """
+        Setup transfer for control use.
+
+        request_type, request, value, index: See USBDeviceHandle.controlWrite.
+        buffer_or_len: either a string (when sending data), or expected data
+          length (when receiving data)
+        callback: function to call upon event. Called with transfer as
+          parameter, return value ignored.
+        user_data: to pass some data to/from callback
+        timeout: in milliseconds, how long to wait for devices acknowledgement
+          or data. Set to 0 to disable.
+        """
+        if self.__submitted:
+            raise ValueError, 'Cannot alter a submitted transfer'
+        if isinstance(buffer_or_len, basestring):
+            length = len(buffer_or_len)
+            string_buffer = create_string_buffer(
+                ' ' * libusb1.LIBUSB_CONTROL_SETUP_SIZE + buffer_or_len)
+        else:
+            length = buffer_or_len
+            string_buffer = create_string_buffer(length + \
+                libusb1.LIBUSB_CONTROL_SETUP_SIZE)
+        libusb1.libusb_fill_control_setup(string_buffer, request_type,
+            request, value, index, length)
+        libusb1.libusb_fill_control_transfer(self.__transfer, self.__handle,
+            string_buffer, self.__ctypesCallbackWrapper, user_data, timeout)
+        self.__callback = callback
+        self.__initialized = True
+
+    def setBulk(self, endpoint, buffer_or_len, callback=None, user_data=None,
+            timeout=0):
+        """
+        Setup transfer for bulk use.
+
+        endpoint: endpoint to submit transfer to (implicitly sets transfer
+          direction).
+        buffer_or_len: either a string (when sending data), or expected data
+          length (when receiving data)
+        callback: function to call upon event. Called with transfer as
+          parameter, return value ignored.
+        user_data: to pass some data to/from callback
+        timeout: in milliseconds, how long to wait for devices acknowledgement
+          or data. Set to 0 to disable.
+        """
+        if self.__submitted:
+            raise ValueError, 'Cannot alter a submitted transfer'
+        string_buffer = create_string_buffer(buffer_or_len)
+        libusb1.libusb_fill_bulk_transfer(self.__transfer, self.__handle,
+            endpoint, string_buffer, sizeof(string_buffer),
+            self.__ctypesCallbackWrapper, user_data, timeout)
+        self.__callback = callback
+        self.__initialized = True
+
+    def setInterrupt(self, endpoint, buffer_or_len, callback=None,
+            user_data=None, timeout=0):
+        """
+        Setup transfer for interrupt use.
+
+        endpoint: endpoint to submit transfer to (implicitly sets transfer
+          direction).
+        buffer_or_len: either a string (when sending data), or expected data
+          length (when receiving data)
+        callback: function to call upon event. Called with transfer as
+          parameter, return value ignored.
+        user_data: to pass some data to/from callback
+        timeout: in milliseconds, how long to wait for devices acknowledgement
+          or data. Set to 0 to disable.
+        """
+        if self.__submitted:
+            raise ValueError, 'Cannot alter a submitted transfer'
+        string_buffer = create_string_buffer(buffer_or_len)
+        libusb1.libusb_fill_interrupt_transfer(self.__transfer, self.__handle,
+            endpoint, string_buffer,  sizeof(string_buffer),
+            self.__ctypesCallbackWrapper, user_data, timeout)
+        self.__callback = callback
+        self.__initialized = True
+
+    def setIsochronous(self, *args, **kw):
+        """
+        Setup transfer for isochronous use.
+        XXX: Not implemented yet
+        """
+        if self.__submitted:
+            raise ValueError, 'Cannot alter a submitted transfer'
+        raise NotImplementedError
+
+    def getStatus(self):
+        """
+        Get transfer status.
+        Should not be called on a submitted transfer.
+        """
+        return self.__transfer.contents.status
+
+    def getActualLength(self):
+        """
+        Get actually transfered data length.
+        Should not be called on a submitted transfer.
+        """
+        return self.__transfer.contents.actual_length
+
+    def getBuffer(self):
+        """
+        Get data buffer content.
+        Should not be called on a submitted transfer.
+        """
+        transfer = self.__transfer.contents
+        result = string_at(transfer.buffer, transfer.length)
+        if transfer.type == libusb1.LIBUSB_TRANSFER_TYPE_CONTROL:
+            result = result[libusb1.LIBUSB_CONTROL_SETUP_SIZE - 1:]
+        return result
+
+    def setBuffer(self, buffer_or_len):
+        """
+        Replace buffer with a new one.
+        Allows resizing read buffer and replacing data sent.
+        """
+        if self.__submitted:
+            raise ValueError, 'Cannot alter a submitted transfer'
+        transfer = self.__transfer.contents
+        if transfer.type == libusb1.LIBUSB_TRANSFER_TYPE_CONTROL:
+            setup = string_at(transfer.buffer,
+                libusb1.LIBUSB_CONTROL_SETUP_SIZE)
+            if isinstance(buffer_or_len, basestring):
+                wLength = len(buffer_or_len)
+                string_buffer = create_string_buffer(setup + buffer_or_len)
+            else:
+                wLength = buffer_or_len
+                string_buffer = create_string_buffer(setup, buffer_or_len + \
+                    libusb1.LIBUSB_CONTROL_SETUP_SIZE)
+            cast(string_buffer, libusb1.libusb_control_setup_p).contents.\
+                wLength = wLength
+        else:
+            string_buffer = create_string_buffer(buffer_or_len)
+        transfer.buffer = string_buffer
+        transfer.length = sizeof(string_buffer)
+
+    def isSubmitted(self):
+        """
+        Tells if this transfer is submitted and still pending.
+        """
+        return self.__submitted
+
+    def submit(self):
+        """
+        Submit a transfer for asynchronous handling.
+        """
+        if not self.__initialized:
+            raise ValueError, 'Cannot submit a transfer until it has been ' \
+                'initialized'
+        if self.__callback is None:
+            raise ValueError, 'A callback must be set on transfer before it ' \
+                'can be submitted'
+        result = libusb1.libusb_submit_transfer(self.__transfer)
+        if result:
+            raise libusb1.USBError, result
+        self.__submitted = True
+
+    def cancel(self):
+        """
+        Cancel given transfer.
+        Note: cancellation happensasynchronously, so you must wait for
+        LIBUSB_TRANSFER_CANCELLED.
+        """
+        result = libusb1.libusb_cancel_transfer(self.__transfer)
+        if result:
+            raise libusb1.USBError, result
+        self.__submitted = False
+
+class USBTransferHelper(object):
+    """
+    Simplifies subscribing to the same transfer over and over, and callback
+    handling.
+
+    Callbacks used in this class must follow the callback API described in
+    USBTransfer, and are expected to return a boolean:
+    - True if transfer is to be submitted again (to receive/send more data)
     - False otherwise
     """
-    _handle = None
-    _submited = False
-
-    def __init__(self, handle, endpoint, size, user_data=None, timeout=0):
+    def __init__(self, transfer):
         """
-        Create an asynchronous reader for given device (handle) and endpoint.
-        size is the size of receiving buffer.
-        user_data is any kind of data you wish to receive in event callbacks.
-        timeout is in milliseconds (0 to disable).
+        Create a helper for given USBTransfer.
+        Note: transfer's callback function is overwritten upon instanciation.
         """
-        data = create_string_buffer(size)
-        self._data = data
-        self._transfer = self._getTransfer(
-          handle,
-          endpoint,
-          data,
-          self._callbackDispatcher,
-          user_data,
-          timeout,
-        )
-        # XXX: set _handle *after* _transfer, so __del__ doesn't get an
-        # exception if called during constructor execution.
-        self._handle = handle
-        self._event_callback_dict = {}
-        self._errorCallback = DEFAULT_ASYNC_TRANSFER_ERROR_CALLBACK
-
-    def _getTransfer(self, handle, endpoint, data, callbackDispatcher,
-            user_data, timeout):
-        raise NotImplementedError
+        self.__transfer = transfer
+        transfer.setCallback(self.__callbackDispatcher)
+        self.__event_callback_dict = {}
+        self.__errorCallback = DEFAULT_ASYNC_TRANSFER_ERROR_CALLBACK
 
     def submit(self):
         """
         Submit the asynchronous read request.
         """
-        self._submited = True
-        self._handle.submitTransfer(self._transfer)
+        self.__transfer.submit()
 
     def cancel(self):
         """
         Cancel a pending read request.
         """
-        self._handle.cancelTransfer(self._transfer)
-        self._submited = False
+        self.__transfer.cancel()
 
     def setEventCallback(self, event, callback):
         """
@@ -108,7 +339,7 @@ class USBAsyncReaderBase(object):
         """
         if event not in EVENT_CALLBACK_SET:
             raise ValueError, 'Unknown event %r.' % (event, )
-        self._event_callback_dict[event] = callback
+        self.__event_callback_dict[event] = callback
 
     def setDefaultCallback(self, callback):
         """
@@ -116,49 +347,32 @@ class USBAsyncReaderBase(object):
         registered.
         The initial default callback does nothing and returns false.
         """
-        self._errorCallback = callback
+        self.__errorCallback = callback
 
     def getEventCallback(self, event, default=None):
         """
         Return the function registered to be called for given event identifier.
         """
-        return self._event_callback_dict.get(event, default)
+        return self.__event_callback_dict.get(event, default)
 
-    def _callbackDispatcher(self, transfer_p):
-        transfer = self._transfer.contents #transfer_p.contents
-        if self.getEventCallback(transfer.status, self._errorCallback)(
-            transfer, self._data):
+    def __callbackDispatcher(self, transfer):
+        assert transfer is self.__transfer
+        if self.getEventCallback(transfer.getStatus(), self.__errorCallback)(
+                transfer):
             self.submit()
-        else:
-            self._submited = False
 
     def isSubmited(self):
         """
         Returns whether this reader is currently waiting for an event.
         """
-        return self._submited
+        return self.__transfer.isSubmitted()
 
     def __del__(self):
-        if self._handle is not None:
-            try:
-                self.cancel()
-            except libusb1.USBError, exception:
-                if exception.value != libusb1.LIBUSB_ERROR_NOT_FOUND:
-                    raise
-
-class USBAsyncBulkReader(USBAsyncReaderBase):
-    """
-    Asynchronous reader for bulk endpoint.
-    """
-    def _getTransfer(self, handle, *args, **kw):
-        return handle.getBulkTransfer(*args, **kw)
-
-class USBAsyncInterruptReader(USBAsyncReaderBase):
-    """
-    Asynchronous reader for interrupt endpoint.
-    """
-    def _getTransfer(self, handle, *args, **kw):
-        return handle.getInterruptTransfer(*args, **kw)
+        try:
+            self.cancel()
+        except libusb1.USBError, exception:
+            if exception.value != libusb1.LIBUSB_ERROR_NOT_FOUND:
+                raise
 
 class USBPoller(object):
     """
@@ -521,71 +735,13 @@ class USBDeviceHandle(object):
         transferred = self._interruptTransfer(endpoint, data, length, timeout)
         return data.raw[:transferred]
 
-    def _getTransfer(self, iso_packets=0):
-        result = libusb1.libusb_alloc_transfer(iso_packets)
-        if not result:
-            raise libusb1.USBError, 'Unable to get a transfer object'
-        return result
-
-    def fillBulkTransfer(self, transfer, endpoint, string_buffer,
-                         callback, user_data, timeout):
-        libusb1.libusb_fill_bulk_transfer(transfer, self.handle,
-            endpoint, string_buffer, sizeof(string_buffer),
-            libusb1.libusb_transfer_cb_fn_p(callback), user_data,
-            timeout)
-
-    def getBulkTransfer(self, endpoint, string_buffer, callback,
-                        user_data=None, timeout=0):
-        result = self._getTransfer()
-        self.fillBulkTransfer(result, endpoint, string_buffer, callback,
-            user_data, timeout)
-        return result
-
-    def fillInterruptTransfer(self, transfer, endpoint, string_buffer,
-                              callback, user_data, timeout):
-        libusb1.libusb_fill_interrupt_transfer(transfer, self.handle,
-            endpoint, string_buffer,  sizeof(string_buffer),
-            libusb1.libusb_transfer_cb_fn_p(callback), user_data,
-            timeout)
-
-    def getInterruptTransfer(self, endpoint, string_buffer, callback,
-                             user_data=None, timeout=0):
-        result = self._getTransfer()
-        self.fillInterruptTransfer(result, endpoint, string_buffer,
-            callback, user_data, timeout)
-        return result
-
-    def fillControlSetup(self, string_buffer, request_type, request, value,
-                         index, length):
-        libusb1.libusb_fill_control_setup(string_buffer, request_type,
-            request, value, index, length)
-
-    def fillControlTransfer(self, transfer, setup, callback,
-                            user_data, timeout):
-        libusb1.libusb_fill_control_transfer(transfer, self.handle,
-            setup, libusb1.libusb_transfer_cb_fn_p(callback), user_data,
-            timeout)
-
-    def getControlTransfer(self, setup, callback, user_data=None, timeout=0):
-        result = self._getTransfer()
-        self.fillControlTransfer(result, setup, callback, user_data, timeout)
-        return result
-
-    def fillISOTransfer(self, *args, **kw):
-        raise NotImplementedError
-
-    def getISOTransfer(self, *args, **kw):
-        raise NotImplementedError
-
-    def submitTransfer(self, transfer):
-        result = libusb1.libusb_submit_transfer(transfer)
-        if result:
-            raise libusb1.USBError, result
-
-    def cancelTransfer(self, transfer):
-        result = libusb1.libusb_cancel_transfer(transfer)
-        if result:
-            raise libusb1.USBError, result
+    def getTransfer(self, iso_packets=0):
+        """
+        Get an empty transfer for asynchronous use.
+        iso_packets: the number of isochronous transfer descriptors to
+          allocate.
+        """
+        return USBTransfer(self.__handle, iso_packets)
 
 class USBDevice(object):
     """
