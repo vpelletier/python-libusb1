ae8d90da3a46c5444f6c62275048eef1fd1d67b0
Vincent Pelletier <plr.vincent@gmail.com>
2011-10-18 23:53:17 +0200

    Add email and licence in setup.py .

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ setup(name='python-libusb1',
       description='a Python wrapper around libusb-1.0',
       version='0.1.2',
       author='Vincent Pelletier',
+      author_email='plr.vincent@gmail.com',
       url='http://github.com/vpelletier/python-libusb1',
+      license='GPL',
       py_modules=['libusb1','usb1'],
       )
