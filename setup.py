648f72c21cc89eb0767f8c2da9aa0c1d1cef7a66
Vincent Pelletier <plr.vincent@gmail.com>
2011-10-24 23:16:26 +0200

    Provide more setup metadata.

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@ from distutils.core import setup
 setup(
     name='libusb1',
     description='Python wrapper around libusb-1.0',
+    keywords='usb libusb',
     version='0.1.2',
     author='Vincent Pelletier',
     author_email='plr.vincent@gmail.com',
     url='http://github.com/vpelletier/python-libusb1',
     license='GPL',
+    platforms=['any'],
     py_modules=['libusb1','usb1'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+    ],
 )
