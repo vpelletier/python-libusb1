5bae1eb9bcace922304767b814117756b0570ac3
Vincent Pelletier <plr.vincent@gmail.com>
2011-10-24 23:15:28 +0200

    Reindent setup.py .

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
-setup(name='libusb1',
-      description='a Python wrapper around libusb-1.0',
-      version='0.1.2',
-      author='Vincent Pelletier',
-      author_email='plr.vincent@gmail.com',
-      url='http://github.com/vpelletier/python-libusb1',
-      license='GPL',
-      py_modules=['libusb1','usb1'],
-      )
+
+setup(
+    name='libusb1',
+    description='Python wrapper around libusb-1.0',
+    version='0.1.2',
+    author='Vincent Pelletier',
+    author_email='plr.vincent@gmail.com',
+    url='http://github.com/vpelletier/python-libusb1',
+    license='GPL',
+    py_modules=['libusb1','usb1'],
+)
