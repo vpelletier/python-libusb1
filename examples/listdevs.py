245628bf53bf7255ccd5aa15d21ff8c1f5751ef8
Vincent Pelletier <plr.vincent@gmail.com>
2013-09-24 19:56:33 +0200

    examples: Modify listdev to exercise getPortNumberList .

--- examples/listdevs.py
+++ examples/listdevs.py
@@ -4,7 +4,7 @@ import usb1
 def main():
     context = usb1.USBContext()
     for device in context.getDeviceList(skip_on_error=True):
-        print str(device)
+        print 'ID %04x:%04x' % (device.getVendorID(), device.getProductID()), '->'.join(str(x) for x in ['Bus %03i' % (device.getBusNumber(), )] + device.getPortNumberList()), 'Device', device.getDeviceAddress()
 
 if __name__ == '__main__':
     main()
