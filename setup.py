48c03a445e88db9c4f9f6652d8590e469d23ea4f
Vincent Pelletier <plr.vincent@gmail.com>
2021-03-13 07:09:48 +0000

    Bump copyright year.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2010-2018  Vincent Pelletier <plr.vincent@gmail.com>
+# Copyright (C) 2010-2021  Vincent Pelletier <plr.vincent@gmail.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
