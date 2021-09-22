e40a3f0854d2070d821eefa07f21e84808fb9793
Vincent Pelletier <plr.vincent@gmail.com>
2021-09-22 00:11:09 +0000

    setup.sh: Drop python2 .

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 # pypy fails when installing the wheel: libusb.py compilation does not produce a .pyc .
-for python in /usr/bin/python2.7 /usr/bin/python3.9 /usr/bin/pypy3; do
+for python in /usr/bin/python3.9 /usr/bin/pypy3; do
   "$(dirname "$(realpath "$0")")"/runTestLibusb.sh "$python" https://github.com/libusb/libusb.git libusb.git master v1.0.19 v1.0.22
 done
 export I_KNOW_HOW_TO_RELEASE_PYTHON_LIBUSB1=1
@@ -9,7 +9,7 @@ echo "Fetching libusb1 windows binary distribution..."
 python3 setup.py --quiet update_libusb
 echo "Building distributions..."
 embedded_dll_path="usb1/libusb-1.0.dll"
-for python_v in python2 python3; do
+for python_v in python3; do
   echo "$python_v bdist_wheel win32"
   cp "build/win32/libusb-1.0.dll" "$embedded_dll_path"
   "${python_v}" setup.py --quiet bdist_wheel --plat-name win32 clean --all
