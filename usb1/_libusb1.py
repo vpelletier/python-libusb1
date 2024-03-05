a03f4c3171d55a8325accc6b79246c235f0a6ce5
Vincent Pelletier <plr.vincent@gmail.com>
2024-03-05 23:20:36 +0000

    usb1._libusb1: Mark libusb_control_setup as packed
    Following a libusb commit: which states that libusb relies on this struct
    not having any padding but until now it was left to the compiler to decide.
    Apparently no compiler (nor ctypes) insert padding as otherwise it would
    have broken all asynchronous control transfers, but better be explicit.

--- usb1/_libusb1.py
+++ usb1/_libusb1.py
@@ -942,6 +942,7 @@ libusb_config_descriptor_p = POINTER(libusb_config_descriptor)
 libusb_config_descriptor_p_p = POINTER(libusb_config_descriptor_p)
 
 class libusb_control_setup(Structure):
+    _pack_ = 1
     _fields_ = [
         ('bmRequestType', c_uint8),
         ('bRequest', c_uint8),
